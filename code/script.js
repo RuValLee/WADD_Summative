48ac71fc1c0129acf9ce5cbf99026128be8afc6e
RuValLee <htvl500@york.ac.uk>
2025-06-17 19:39:26 +0100

    Character sprites created, preloaded into game canvas but not displayed

--- code/script.js
+++ code/script.js
@@ -1,15 +1,27 @@
 let backgroundMap;
+let bodySprite, hairSprite, bodyFrames, hairFrames;
+const spriteWidth = 96;
 
 function preload() {
     backgroundMap = loadImage("../assets/backgroundMap.png");
+    // bodySprite = loadImage("../assets/base_walk_strip8.png");
+    // hairSprite = loadImage("../assets/Characters.png");
 }
 
 function setup() {
     const canvas = createCanvas(640, 640);
     canvas.id("gameCanvas");
-    image(backgroundMap, 0, 0, 640, 640);
 }
 
 function draw() {
+    image(backgroundMap, 0, 0, 640, 640);
+}
 
+function getSprite(spritePng, startingFrame, endingFrame, animationArray) {
+    const totalFrames = 8;
+    for(let i = startingFrame; i < totalFrames; i++) {
+        let img = spritePng.get(pos.x, pos.y, pos.w, pos.h);
+        animationArray.push(img);
+    }
+    return animationArray;
 }
