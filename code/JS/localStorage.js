0f7acecd8f86d87e4cef126f25bb8191ffcfff0f
RuValLee <htvl500@york.ac.uk>
2025-06-24 18:46:42 +0100

    Functions for saving, loading, clearing localStorage added, script linked to game.index

---
+++ code/JS/localStorage.js
@@ -0,0 +1,23 @@
+function saveGameState() {
+    const gameState = {
+        currentDay,
+        totalOptionCount,
+        dailyChoices
+    };
+    localStorage.setItem("hereStillSavedData", JSON.stringify(gameState));
+}
+
+function loadGameState() {
+    const savedData = localStorage.getItem("hereStillSavedData");
+
+    if(savedData) {
+        const gameState = JSON.parse(savedData);
+        currentDay = gameState.currentDay;
+        totalOptionCount = gameState.totalOptionCount;
+        dailyChoices = gameState.dailyChoices;
+    }
+}
+
+function clearGameStates() {
+    localStorage.removeItem("hereStillSavedData");
+}
